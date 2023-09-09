23dbadf99017e94126f0ba4915e2a1fe1c968f21
ukikagi <ukikagi@gmail.com>
2023-09-09 19:10:25 +0900

    Add very naive UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { generateParty } from "./generateParty";
+
+interface ResultProp {
+  result: Array<[string, Array<string>]> | null;
+}
+
+const Result = ({ result }: ResultProp) => {
+  if (result === null) {
+    return <div>生成できませんでした</div>;
+  }
+  let ls = [];
+  for (const idx in result) {
+    const [split, examples] = result[idx];
+    ls.push(
+      <div key={idx}>
+        {split} =&gt; {JSON.stringify(examples)}{" "}
+      </div>
+    );
+  }
+  return <>{ls}</>;
+};
 
 function App() {
+  const [partyName, setPartyName] = useState("");
+  const [useOnlyPrefix, setUseOnlyPrefix] = useState(false);
+  const [result, setResult] = useState<Array<[string, Array<string>]> | null>([]);
+  const handleClick = () => {
+    setResult(generateParty(partyName, 1, useOnlyPrefix, 3, 6));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        パーティー名:{" "}
+        <input
+          type="text"
+          value={partyName}
+          onChange={(e) => setPartyName(e.target.value)}
+        />
+      </label>
+      <hr />
+      <label>
+        接頭辞のみを使う:{" "}
+        <input
+          type="checkbox"
+          checked={useOnlyPrefix}
+          onChange={(e) => setUseOnlyPrefix(e.target.checked)}
+        />
+      </label>
+      <hr />
+      <button onClick={handleClick}>生成</button>
+      <hr />
+      <Result result={result} />
     </div>
   );
 }
