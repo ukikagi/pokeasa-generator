2ba20085cbf52b60055e392d3e3c6d5a12af1026
ukikagi <ukikagi@gmail.com>
2023-09-09 19:46:31 +0900

    Updates

---
+++ precache-manifest.91899331a7751b17873af778e10e5fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1ca1bd65c0b8250af99",
+    "url": "/pokeasa-generator/static/css/main.a77b03d3.chunk.css"
+  },
+  {
+    "revision": "d1ca1bd65c0b8250af99",
+    "url": "/pokeasa-generator/static/js/main.d1ca1bd6.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/pokeasa-generator/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "9768bc59323b3f73ab50",
+    "url": "/pokeasa-generator/static/js/2.9768bc59.chunk.js"
+  },
+  {
+    "revision": "8b3e74e9870b13e9e8c7",
+    "url": "/pokeasa-generator/static/js/runtime~main.8b3e74e9.js"
+  },
+  {
+    "revision": "0c19c0b3253f3b39a3b48a78ea037398",
+    "url": "/pokeasa-generator/index.html"
+  }
+];
